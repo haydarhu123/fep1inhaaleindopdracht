80f3cf8ab8e4f2b8d3ccee6624e7a082ac06e093
haydarhu123 <60918869+haydarhu123@users.noreply.github.com>
2022-02-10 13:21:01 +0100

    final commit searchbar toegevoegd

--- public/Mario.js
+++ public/Mario.js
@@ -4,51 +4,72 @@ const API_URL = `https://www.amiiboapi.com/api/amiibo`;
         res => {
           res.json().then(
             data => {
-              console.log(data.amiibo);
-              
-              if (data.amiibo.length > 0) {
+             displayData(data.amiibo)
+             const searchBar = document.getElementById('searchBar');
 
-                var bt = document.getElementById("alledata").cloneNode();
-                var temp = "";
-                data.amiibo.forEach((itemData) => {
-                  temp += "<tr>";
-                  temp += '<td><img src="'+itemData.image+'"></td>';
-                  temp += "<td>" + itemData.amiiboSeries + "</td>";
-                  temp += "<td>" + itemData.character + "</td>";
-                  temp += '<td><button onclick ="openInformatie(\''+itemData.image+'\')" ">Meer informatie</td></tr>';
-
-                  temp += '<tr class =popup id="'+itemData.image+'" >';
-                  temp += '<td>foto: <img src="'+itemData.image+'"></td>';
-                  temp += "<td>amibo serie: " + itemData.amiiboSeries + "</td>";
-                  temp += "<td>karakter:" + itemData.character + "</td>";
-                  temp += "<td>game serie: " + itemData.gameSeries + "</td>";
-                  temp += "<td>releasedatum Europa: " + itemData.release.eu + "</td>";
-                  temp += "<td>type: " + itemData.type + "</td>";
-                  
-                  temp += '<td><button onclick =" closeInformatie(\''+itemData.image+'\')" ">Sluiten</td></tr>';
-
-                //   document.getElementById(itemData.image).addEventListener("click", openInformatie(itemData.image));
-                });
-                document.getElementById('data').innerHTML = temp;
-                
-              }
+// hier word alles omgezet naar kleine letters wat de gebruiker intoetst.
+             searchBar.addEventListener('keyup', (e) => {
+                 const searchString = e.target.value.toLowerCase();
+                 
+                 // hier filter ik op karakter, gameserie en amiboserie
+                 const filteredCharactersandseries = data.amiibo.filter((c) => {
+                     return (
+                         c.character.toLowerCase().includes(searchString) ||
+                         c.amiiboSeries.toLowerCase().includes(searchString) ||
+                         c.gameSeries.toLowerCase().includes(searchString)
+                     );
+                 });
+                 // hier word worden de gefilterde keuzes weergeven.
+                 displayData(filteredCharactersandseries);
+             });
             }
           )
         }
       )
 
-      function openInformatie(image){
+// voor het openen en sluiten van de pop-up geef ik de image mee als parameter, 
+// dit is het enige wat bij iedere object binnen de api verschillend is overal.
 
+// functie voor het openen van de pop-up zodra er geklikt wordt op de button 'Meer Informatie'.
+function openInformatie(image){
   const informatie = document.getElementById(image);
-  if(informatie !== null){
-      console.log("clicked")
     informatie.style.display = "block";
-  }
-
 }
      
+// functie voor het sluiteb van de pop-up zodra er geklikt wordt op de button 'Sluiten' binnen de pop-up.
 function closeInformatie(image){
         const toelichting = document.getElementById(image);
-
         toelichting.style.display = "none";
     }
+
+   
+// dit is een funcie die alle data uit de api toont.
+function displayData(data){
+  console.log(data);
+              
+  if (data.length > 0) {
+
+    var temp = "";
+    data.forEach((itemData) => {
+      temp += "<tr>";
+      temp += '<td><img src="'+itemData.image+'"></td>';
+      temp += "<td>" + itemData.amiiboSeries + "</td>";
+      temp += "<td>" + itemData.character + "</td>";
+      temp += '<td><button onclick ="openInformatie(\''+itemData.image+'\')" ">Meer informatie</td></tr>';
+
+      temp += '<tr class =popup id="'+itemData.image+'" >';
+      temp += '<td>foto: <img src="'+itemData.image+'"></td>';
+      temp += "<td>amibo serie: " + itemData.amiiboSeries + "</td>";
+      temp += "<td>karakter:" + itemData.character + "</td>";
+      temp += "<td>game serie: " + itemData.gameSeries + "</td>";
+      temp += "<td>releasedatum Europa: " + itemData.release.eu + "</td>";
+      temp += "<td>type: " + itemData.type + "</td>";
+      
+      temp += '<td><button onclick =" closeInformatie(\''+itemData.image+'\')" ">Sluiten</td></tr>';
+
+
+    });
+    document.getElementById('data').innerHTML = temp;
+    
+  }
+}
